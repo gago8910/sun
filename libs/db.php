fe508f19393d2ccfd88dcae2c197dc194c7f0808
gago8910 <hant8910@gmail.com>
2015-05-19 09:37:23 +0700

    db libs/db.php

--- libs/db.php
+++ libs/db.php
@@ -1,13 +1,97 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once 'constants.php';
 
-class DB{
-    static function db_connect($sqlServer, $sqlUser, $sqlPassword, $dbName) {
-        
+class DB {
+
+    protected $dbh;
+    static $dbResult;
+    private $error;
+    private $stmt;
+
+    public function __construct($dbServer, $dbHost, $dbName, $dbUsername, $dbPassword) {
+        $dbServer = DB_SERVER;
+        $dbHost = DB_HOST;
+        $dbName = DB_NAME;
+        $dbUsername = DB_USERNAME;
+        $dbPassword = DB_PASSWORD;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        try {
+            $this->dbh = new PDO($dbServer . ':host=' . $dbHost . ';dbname=' . $dbName, $dbUsername, $dbPassword);
+        } catch (PDOException $exc) {
+            $this->error = $exc->getMessage();
+        }
     }
+
+    public function query($query) {
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null) {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function rowCount() {
+        return $this->stmt->rowCount();
+    }
+
+    public function lastInsertId() {
+        return $this->dbh->lastInsertId();
+    }
+
+    public function beginTransaction() {
+        return $this->dbh->beginTransaction();
+    }
+
+    public function endTransaction() {
+        return $this->dbh->commit();
+    }
+
+    public function cancelTransaction() {
+        return $this->dbh->rollBack();
+    }
+
+    public function debugDumpParams() {
+        return $this->stmt->debugDumpParams();
+    }
+
 }
